4b6640d9fcc80618553c2d65dfc93163348cd585
ivanbiljan <ssbiljan@gmail.com>
2024-04-16 17:10:08 +0200

    Add Diagnostics.ProblemReporter

---
+++ src/Darwin/Diagnostics/ProblemReporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Darwin.Diagnostics;
+
+public sealed class ProblemReporter
+{
+    private readonly List<Problem> _problems = [];
+
+    public void AddError(int lineNumber, string description)
+    {
+        _problems.Add(new Problem(lineNumber, description, false));
+    }
+
+    public IEnumerable<Problem> GetAll()
+    {
+        return _problems;
+    }
+}
